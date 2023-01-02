03246661e9c00793578f5eb6686a93274ec88650
Evgenia <106289277+CryParis@users.noreply.github.com>
2023-01-02 23:10:38 +1000

    Create ml.py

---
+++ ml.py
@@ -0,0 +1,16 @@
+import numpy as np 
+
+def sigmoids(x):
+	return 1 / (1 + np.exp(-x))
+
+training_inputs = np.array([[0,0,1],
+						   [1,1,1],
+						   [1,0,1],
+						   [0,1,1]])
+
+training_outputs = np.array([[0,1,1,0]]).T
+
+np.random.seed(1)
+
+synaptic_weights =  np.random.random((3,1)) - 1
+print(synaptic_weights)
